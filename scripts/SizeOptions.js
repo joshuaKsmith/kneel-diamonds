1d0c7f21d1ec8ee6f2c4f9e3b3997cd3dbcda57a
unknown <mtgmirage@protonmail.com>
2024-10-02 14:42:34 -0500

    added options and updated main

--- scripts/SizeOptions.js
+++ scripts/SizeOptions.js
@@ -1,8 +1,13 @@
 export const SizeOptions = async () => {
     const response = await fetch("http://localhost:8088/sizes")
+    const sizes = await response.json()
 
-    // Fill in the rest
+    let sizesHTML = "<h2>Sizes</h2>"
+    for (const size of sizes) {
+        sizesHTML += `
+            <input type='radio' name='size' value='${size.id}' /> ${size.carets}
+        `
+    }
 
-
-    return optionsHTML
+    return sizesHTML
 }
--- scripts/StyleOptions.js
+++ scripts/StyleOptions.js
@@ -1,8 +1,13 @@
 export const StyleOptions = async () => {
     const response = await fetch("http://localhost:8088/styles")
+    const styles = await response.json()
 
-    // Fill in the rest
+    let stylesHTML = "<h2>Styles</h2>"
+    for (const style of styles) {
+        stylesHTML += `
+            <input type='radio' name='style' value='${style.id}' /> ${style.style}
+        `
+    }
 
-
-    return optionsHTML
+    return stylesHTML
 }
